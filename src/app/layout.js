8f9a9d418053984be7445dcb8579ce64dca7e094
Muhammad Aaqib <162022683+aqibaa@users.noreply.github.com>
2026-04-11 12:59:06 +0500

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -5,6 +5,7 @@ import Footer from "@/components/shared/Footer";
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from "@/components/ui/sonner"
 import WishlistSync from "@/components/shared/WishlistSync";
+import { Suspense } from "react";
 
 const inter = Inter({ subsets: ["latin"], variable: '--font-inter' });
 const playfair = Playfair_Display({ subsets: ["latin"], variable: '--font-playfair' });
@@ -24,7 +25,9 @@ export default function RootLayout({ children }) {
     <ClerkProvider>
       <html lang="en" className={`${inter.variable} ${playfair.variable}`}>
         <body className="antialiased font-sans min-h-screen flex flex-col">
-          <Header />
+         <Suspense fallback={<div className="h-16 border-b bg-white"></div>}>
+               <Header />
+             </Suspense>    
           <WishlistSync />
           <main className="flex-1 wrapper">
             {children}
